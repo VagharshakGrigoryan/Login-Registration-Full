2d2ccefd1ae1383f4b817952fe3b6b1d0d91ba4c
Vgharshak <84949833+VagharshakGrigoryan@users.noreply.github.com>
2022-06-03 19:21:01 +0400

    add book and author component

--- login-register-server/src/main/java/com/example/loginregisterserver/security/WebSecurityConfig.java
+++ login-register-server/src/main/java/com/example/loginregisterserver/security/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/user/**").hasAnyRole()
+                .antMatchers("/user/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
